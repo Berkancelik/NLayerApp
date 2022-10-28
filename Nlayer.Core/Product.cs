c5be998d663a2cccb9fddf1b1d0251cb4e7b0bcc
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-10-28 20:19:11 +0300

    relationships between classes

--- Nlayer.Core/Product.cs
+++ Nlayer.Core/Product.cs
@@ -12,5 +12,7 @@ namespace Nlayer.Core
         public int Stock { get; set; }
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ProductFeature ProductFeature { get; set; }
     }
 }
