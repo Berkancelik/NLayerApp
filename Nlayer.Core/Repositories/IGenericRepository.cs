47f49d4f6579f15ec31bb51ab064800e1f3fd10c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-11-02 00:15:12 +0300

    Some Fixes

--- Nlayer.Core/Repositories/IGenericRepository.cs
+++ Nlayer.Core/Repositories/IGenericRepository.cs
@@ -11,7 +11,7 @@ namespace NLayer.Core.Repositories
     {
         Task<T> GetByIdAsync(int id);
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
-        IQueryable<T> GetAlll(Expression<Func<T, bool>> expression);
+        IQueryable<T> GetAll();
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
         Task AddAsync(T entity);
         Task AddRangeAsync(IEnumerable<T> entities);
