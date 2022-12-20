59dcf3f68cbb4bb8beee5c9a71ee307d7659da1a
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-12-20 22:01:49 +0300

    Remove Method has been completed

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -75,5 +75,13 @@ namespace NLayer.Web.Controllers
             return View(productDto);
 
         }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _services.GetByIdAsync(id);
+
+            await _services.RemoveAsync(product);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
