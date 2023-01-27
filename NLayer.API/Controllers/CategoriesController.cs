f6ca995df31573de7d1282817ac71876a59a0aeb
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2023-01-27 22:03:54 +0300

    CaregoryApiService Completed

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
 using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
@@ -8,16 +10,26 @@ namespace NLayer.API.Controllers
     public class CategoriesController : CustomBaseController
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public CategoriesController(ICategoryService categoryService)
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
         }
 
-        [HttpGet("[action]")]
+        [HttpGet]
+        public async Task<IActionResult> GetAl()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200, categoriesDto));
+        }
+
+        [HttpGet("[action]/{categoryId}")]
         public async Task<IActionResult> GetSingleCategoryByIdWithProductAsync(int categoryId)
         {
-            return CreateActionResult(await _categoryService.GetSingleCategoryBtIdWithProductAsync(categoryId));
+            return CreateActionResult(await _categoryService.GetSingleCategoryByIdWithProductsAsync(categoryId));
         }
 
      
--- NLayer.Web/Services/CategoryApiService.cs
+++ NLayer.Web/Services/CategoryApiService.cs
@@ -1,12 +1,19 @@
-﻿namespace NLayer.Web.Services
+﻿using NLayer.Core.DTOs;
+
+namespace NLayer.Web.Services
 {
     public class CategoryApiService
     {
         private readonly HttpClient _httpClient;
 
-        public ProductApiService(HttpClient httpClient)
+        public CategoryApiService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+        public async Task<List<CategoryDto>> GetAllASync()
+        {
+            var response = await _httpClient.GetFromJsonAsync< CustomResponseDto<List<CategoryDto>>>("categories");
+            return response.Data;
         }
     }
 }
